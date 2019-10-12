075345b0bea54c54e422c56aaa4b6aab92bd8fc8
Oleg Stotsky <ostotsky@gmail.com>
2019-10-12 14:24:50 +0300

    update command line argument parsing

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@ struct args {
     bool is_verbose;
     bool compress;
     bool decompress;
+    std::string in_file_name;
+    std::string out_file_name;
 };
 
 args parse_args(int argc, char **argv) {
+    if (argc < 4) {
+        throw std::invalid_argument("usage: huffman [-v] (-c|-d) input_file output_file");
+    }
     bool is_v = false;
     bool is_compress = false;
     bool is_decompress = false;
+    std::string in_file_name = std::string(argv[argc - 2]);
+    std::string out_file_name = std::string(argv[argc - 1]);
 
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc - 2; ++i) {
         if (strcmp(argv[i], "-v") == 0) {
             is_v = true;
         }
@@ -34,16 +41,14 @@ args parse_args(int argc, char **argv) {
         throw std::invalid_argument("at least one of -c -d options should be passed");
     }
 
-    return {is_v, is_compress, is_decompress};
+    return {is_v, is_compress, is_decompress, in_file_name, out_file_name};
 }
 
 int main(int argc, char **argv) {
-    using namespace std;
-
     try {
         args arguments = parse_args(argc, argv);
     } catch (std::invalid_argument e) {
-        cerr << e.what();
+        std::cerr << e.what();
     }
 
     return 0;
