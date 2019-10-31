32eeb45a4bef92c38ed8b27eacfc8eae1b78f4fd
Oleg Stotsky <ostotsky@gmail.com>
2019-10-31 15:03:14 +0300

    use std::variant for error handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <variant>
 #include "huffman.hpp"
 
 struct args {
@@ -10,9 +11,9 @@ struct args {
     std::string out_file_name;
 };
 
-args parse_args(int argc, char **argv) {
+std::variant<std::string, args> parse_args(int argc, char **argv) {
     if (argc < 4) {
-        throw std::invalid_argument("usage: huffman [-v] (-c|-d) input_file output_file");
+        return "usage: huffman [-v] (-c|-d) input_file output_file";
     }
     bool is_v = false;
     bool is_compress = false;
@@ -21,44 +22,44 @@ args parse_args(int argc, char **argv) {
     std::string out_file_name = std::string(argv[argc - 1]);
 
     for (int i = 1; i < argc - 2; ++i) {
-        if (std::string(argv[i]) ==  "-v") {
+        if (std::string(argv[i]) == "-v") {
             is_v = true;
-        }
-        if (std::string(argv[i]) ==  "-c") {
+        } else if (std::string(argv[i]) == "-c") {
             if (is_decompress) {
-                throw std::invalid_argument("-c -d options can't be used simultaneously");
+                return "-c -d options can't be used simultaneously";
             }
             is_compress = true;
-        }
-        if (std::string(argv[i]) == "-d") {
+        } else if (std::string(argv[i]) == "-d") {
             if (is_compress) {
-                throw std::invalid_argument("-c -d options can't be used simultaneously");
+                return "-c -d options can't be used simultaneously";
             }
             is_decompress = true;
+        } else {
+            return "unknown option: " + std::string(argv[i]);
         }
     }
 
     if (!is_compress && !is_decompress) {
-        throw std::invalid_argument("at least one of -c -d options should be passed");
+        return "at least one of -c -d options should be passed";
     }
 
-    return {is_v, is_compress, in_file_name, out_file_name};
+    return args{is_v, is_compress, in_file_name, out_file_name};
 }
 
 int main(int argc, char **argv) {
-    try {
-        args arguments = parse_args(argc, argv);
-        std::ifstream in_file(arguments.in_file_name, std::ios::binary);
-        std::ofstream out_file(arguments.out_file_name, std::ios::binary);
-        if (arguments.compress) {
-            compress(in_file, out_file, arguments.is_verbose);
-        } else {
-            decompress(in_file, out_file, arguments.is_verbose);
-        }
-
-        return 0;
-    } catch (std::exception &e) {
-        std::cerr << e.what();
+    auto parse_result = parse_args(argc, argv);
+    if (const auto error = std::get_if<0>(&parse_result)) {
+        std::cerr << *error;
         return 1;
     }
+    args arguments = std::get<args>(parse_result);
+    std::ifstream in_file(arguments.in_file_name, std::ios::binary);
+    std::ofstream out_file(arguments.out_file_name, std::ios::binary);
+    if (arguments.compress) {
+        compress(in_file, out_file, arguments.is_verbose);
+    } else {
+        decompress(in_file, out_file, arguments.is_verbose);
+    }
+
+    return 0;
 }
